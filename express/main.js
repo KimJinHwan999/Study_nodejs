3ee3146c4047c18c55a90b7d38f2301ecc634b17
KimJinHwan999 <wlsghks5438@gmail.com>
2023-01-20 02:50:18 +0900

    express 공부1

--- express/main.js
+++ express/main.js
@@ -15,7 +15,13 @@ var template = require('./lib/template.js');
 // 1. 사용자가 요청할 때 마다 바디파서 미들웨어가 실행 됨 
 // 2. post 데이터를 내부적으로 분석해서 가져온 다음에 콜백을 호출하도록 약속
 // 3. 콜백의 request.body 프로퍼티를 만들어줌
+
+// form데이터는 이렇게 처리
 app.use(bodyParser.urlencoded({ extended: false })); 
+
+// json으로 처리할 땐
+// app.use(bodyparser.json())
+
 app.use(compression()); // 우리 어플리케이션은 요청이 들어올 때마다 bodyParser 미들웨어와 compression 미들웨어가 실행됨
 app.get('*', function(request, response, next){ // get으로 들어올 때만 파일 목록을 가져오게 됨
   fs.readdir('./data', function(error, filelist){
